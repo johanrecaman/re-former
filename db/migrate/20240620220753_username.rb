27aab18f3f3d9b55e885c0757270a16daceaa4c2
Johan Recaman <johanstrr@gmail.com>
2024-06-23 19:11:18 -0300

    using form_with

---
+++ db/migrate/20240620220753_username.rb
@@ -0,0 +1,4 @@
+class Username < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_074936) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_221104) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
